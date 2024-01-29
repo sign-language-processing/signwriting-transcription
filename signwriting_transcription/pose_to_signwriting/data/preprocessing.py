343f1e0c0ea7fae8fab752717f19fb408a00d1f3
RotemZilberman <rotemzil2203@gmail.com>
2024-01-29 09:35:34 +0200

    fixing preprocessing

--- signwriting_transcription/pose_to_signwriting/data/preprocessing.py
+++ signwriting_transcription/pose_to_signwriting/data/preprocessing.py
@@ -14,10 +14,11 @@ def preprocess(src_dir, trg_dir, action=True):
     for path in tqdm(src_dir.glob("*.pose")):
         with open(src_dir / path.name, 'rb') as pose_file:
             pose = Pose.read(pose_file.read())
-            pose = reduce_holistic(pose)
         if action:
             pose = pre_process_mediapipe(pose)
             pose = normalize_mean_std(pose)
+        else:
+            pose = reduce_holistic(pose)
         with open(trg_dir / path.name, 'wb') as pose_file:
             pose.write(pose_file)
 
