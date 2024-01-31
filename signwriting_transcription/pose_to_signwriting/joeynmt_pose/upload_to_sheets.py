555bd1afe94e34845fc4cd6f1d1dd099f1268cc1
RotemZilberman <rotemzil2203@gmail.com>
2024-02-01 01:29:41 +0200

    zero augmentation + zero normalization + zero noise - 15 epochs

--- signwriting_transcription/pose_to_signwriting/joeynmt_pose/upload_to_sheets.py
+++ signwriting_transcription/pose_to_signwriting/joeynmt_pose/upload_to_sheets.py
@@ -23,19 +23,19 @@ def upload_line(new_row):
     # The file token.json stores the user's access and refresh tokens, and is
     # created automatically when the authorization flow completes for the first
     # time.
-    if os.path.exists("token.json"):
-        creds = Credentials.from_authorized_user_file("token.json", SCOPES)
+    if os.path.exists("signwriting_transcription/pose_to_signwriting/joeynmt_pose/token.json"):
+        creds = Credentials.from_authorized_user_file("signwriting_transcription/pose_to_signwriting/joeynmt_pose/token.json", SCOPES)
     # If there are no (valid) credentials available, let the user log in.
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
         else:
             flow = InstalledAppFlow.from_client_secrets_file(
-                "credentials.json", SCOPES
+                "signwriting_transcription/pose_to_signwriting/joeynmt_pose/credentials.json", SCOPES
             )
             creds = flow.run_local_server(port=0)
         # Save the credentials for the next run
-        with open("token.json", "w") as token:
+        with open("signwriting_transcription/pose_to_signwriting/joeynmt_pose/token.json", "w") as token:
             token.write(creds.to_json())
 
     try:
