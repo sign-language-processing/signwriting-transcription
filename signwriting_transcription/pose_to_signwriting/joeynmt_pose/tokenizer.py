1843685330780f5680e9ce155d42bd88a5eefc5a
RotemZilberman <rotemzil2203@gmail.com>
2024-07-13 16:35:32 +0300

    remove small prints

--- signwriting_transcription/pose_to_signwriting/joeynmt_pose/tokenizer.py
+++ signwriting_transcription/pose_to_signwriting/joeynmt_pose/tokenizer.py
@@ -120,8 +120,6 @@ class PoseProcessor(SpeechProcessor):
                     features = pose_ndarray_to_matrix(features, start - start_addition, fps, end + end_reduction)
                 else:
                     features = metadata
-            else:
-                print("hi")
         else:
             raise ValueError("Invalid format")
 
